51ddf9d534bdfab17be87956efea8c70cc81a60a
Никита <42294143+LatyshevNA@users.noreply.github.com>
2019-03-05 12:29:58 +0300

    Add files via upload

---
+++ php/wi-feedback-conf.php
@@ -0,0 +1,8 @@
+<?php
+
+$mail_addr  = 'latushevn.a@yandex.ru';//Адрес получателя
+$mail_topic = 'Поступила заявка с сайта DOMAIN.by';//Тема письма
+$mail_from  = 'informer@DOMAIN.by';//Адрес отправителя указываем свой домен на котором лежит сайт
+$mail_text  = '';
+
+?>
